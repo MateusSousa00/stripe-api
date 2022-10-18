c3ea4935135e8a131c3e4858e9ddf4cc0acce2ae
Mateus Lima <mateus.lsousa00@gmail.com>
2022-10-18 15:37:35 -0300

    fixed env.example

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,16 @@ const stripe = new Stripe(process.env.STRIPE_KEY as string, {apiVersion: '2022-0
 app.post("/checkout", async (req, res, next) => {
   try {
       const session = await stripe.checkout.sessions.create({
+      currency: 'brl',
       payment_method_types: ['card'],
       shipping_address_collection: {
       allowed_countries: ['BR'],
       },
          line_items: [
           {
-            price: req.body.price,
+            price: "5000",
             quantity: 1,
+
           }
          ],
          mode: "subscription",
